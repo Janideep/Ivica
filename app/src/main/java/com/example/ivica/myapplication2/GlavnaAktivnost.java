a2972ffbefb93f46940018d302349cb2d01786d5
Ivica <ivica.vidic@gmail.com>
2016-11-11 11:05:44 +0100

    tesni

---
+++ app/src/main/java/com/example/ivica/myapplication2/GlavnaAktivnost.java
@@ -0,0 +1,13 @@
+package com.example.ivica.myapplication2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GlavnaAktivnost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_glavna_aktivnost);
+    }
+}
